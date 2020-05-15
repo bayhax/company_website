ea58a349f0afd97b14f77a6e65ac4682605b1044
bayhax <whlbayhax@163.com>
2020-05-15 17:24:48 +0800

    settings配置

--- company_website/settings.py
+++ company_website/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'company_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'company',
+        'USER': 'root',
+        'PASSWORD': 'P@ssw0rd1',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
@@ -109,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
