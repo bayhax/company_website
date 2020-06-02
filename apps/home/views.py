acb5ebae9f2f955c482fcc749f4b52c751aa16c4
bayhax <whlbayhax@163.com>
2020-06-02 13:34:03 +0800

    tinymce图片中文路径/资讯图片归类

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,10 +4,13 @@ import time
 
 from PIL import Image
 import matplotlib.pyplot as plt
-from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
+
+from news.models import News
 # from numpy import unicode
 
 # Create your views here.
@@ -37,21 +40,32 @@ def dispute(request):
 
 # 上传图片
 def upload_image(request):
-    # 图片
+    # 上传图片的内容
     file = request.FILES['file']
+    # 获取资讯id
+    news_id = request.META['HTTP_REFERER'].split('/')[6]
     data = {
         'error': True,
-        'path': '',
+        'location': '',
     }
+    filename = str(file)
+    filename_new = filename
     if file:
         try:
             img = Image.open(file)
-            filename = str(file)
             # print(img.size, img.mode, img.format)
-            plt.imshow(img)
-            img.save('./apps/home/static/media/upload/' + filename)
+            # plt.imshow(img)
+            if not os.path.exists('./apps/home/static/media/upload/%s' % news_id):
+                os.mkdir('./apps/home/static/media/upload/%s' % news_id)
+            num = 0
+            while True:
+                if filename_new not in os.listdir('./apps/home/static/media/upload/%s' % news_id):
+                    break
+                filename_new = filename.split('.')[0] + '_%d.' % num + filename.split('.')[1]
+                num += 1
+            img.save('./apps/home/static/media/upload/%s/%s' % (news_id, filename_new))
         except Exception as e:
             print(e)
         data['error'] = False
-        data['location'] = ('../../../../../static/media/upload/%s' % filename)
-    return HttpResponse(json.dumps(data), content_type="application/json")
+        data['location'] = ('../../../../../static/media/upload/%s/%s' % (news_id, filename_new))
+    return HttpResponse(json.dumps(data), content_type="application/json; charset='utf-8'")
